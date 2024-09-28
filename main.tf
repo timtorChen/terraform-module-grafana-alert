87b5c5b715030b3ffe0e17615b3338282bc6ba46
Timtor Chen <to@timtor.dev>
2024-09-28 23:38:53 +0800

    fix: folder_prefix variable

--- main.tf
+++ main.tf
@@ -7,27 +7,27 @@ module "prometheus" {
   labels         = var.prom_labels
 
   enable_ceph_alert = var.enable_ceph_alert
-  ceph_folder_name  = var.ceph_folder_name
+  ceph_folder_name  = "${var.folder_prefix}${var.ceph_folder_name}"
   ceph_rule_groups  = var.ceph_rule_groups
   ceph_selectors    = var.ceph_selectors
 
   enable_etcd_alert = var.enable_etcd_alert
-  etcd_folder_name  = var.etcd_folder_name
+  etcd_folder_name  = "${var.folder_prefix}${var.etcd_folder_name}"
   etcd_rule_groups  = var.etcd_rule_groups
   etcd_selectors    = var.etcd_selectors
 
   enable_kubernetes_alert = var.enable_kubernetes_alert
-  kubernetes_folder_name  = var.kubernetes_folder_name
+  kubernetes_folder_name  = "${var.folder_prefix}${var.kubernetes_folder_name}"
   kubernetes_rule_groups  = var.kubernetes_rule_groups
   kubernetes_selectors    = var.kubernetes_selectors
 
   enable_node_alert = var.enable_node_alert
-  node_folder_name  = var.node_folder_name
+  node_folder_name  = "${var.folder_prefix}${var.node_folder_name}"
   node_rule_groups  = var.node_rule_groups
   node_selectors    = var.node_selectors
 
   enable_smartctl_alert = var.enable_smartctl_alert
-  smartctl_folder_name  = var.smartctl_folder_name
+  smartctl_folder_name  = "${var.folder_prefix}${var.smartctl_folder_name}"
   smartctl_rule_groups  = var.smartctl_rule_groups
   smartctl_selectors    = var.smartctl_selectors
 }
@@ -40,7 +40,7 @@ module "aws" {
   contact_point    = var.aws_contact_point
   labels           = var.aws_labels
   enable_efs_alert = var.enable_efs_alert
-  efs_folder_name  = var.efs_folder_name
+  efs_folder_name  = "${var.folder_prefix}${var.efs_folder_name}"
   efs_rule_groups  = var.efs_rule_groups
 }
 
